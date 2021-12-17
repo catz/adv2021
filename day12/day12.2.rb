fccc738304574af75d074b577f89ffe2e3d1159a
Roman Truschev <truschev@gmail.com>
2021-12-17 20:19:59 +0300

    day12 solution

---
+++ day12/day12.2.rb
@@ -0,0 +1,70 @@
+file = File.open("input12.txt")
+$links = file.readlines.map{|line|
+  line.chomp.split "-"
+}
+
+def small_cave? cave
+  cave.downcase == cave
+end
+
+additional_links = []
+$links.each{|start_cave, end_cave|
+  next if start_cave =="start" || end_cave == "end"
+
+  additional_links << [end_cave, start_cave]
+}
+
+$links += additional_links
+p $links
+
+def connected_caves node
+  $links.select{|start_cave, _| start_cave == node}.map{|n| n[1]}
+end
+
+$paths = []
+
+def visit_small_twice path
+  repeats = path.select{|s| s.downcase == s}.tally.values.tally
+
+  # After reviewing the available paths, you realize you might have time to
+  # visit a single small cave twice. Specifically, big caves can be visited any
+  # number of times, a single small cave can be visited at most twice, and the
+  # remaining small caves can be visited at most once. However, the caves named start
+  # and end can only be visited exactly once each: once you leave the start cave, you may not
+  # return to it, and once you reach the end cave, the path must end immediately.
+  repeats[3] && repeats[3] > 0 || repeats[2] && repeats[2]>1
+end
+
+def traverse current_path, next_caves = [], level=0
+  # p "level: #{level} next_caves: #{next_caves.join(',')}"
+  unless next_caves.empty?
+    next_cave = next_caves.pop
+    path = current_path + [next_cave]
+    if next_cave == "start"
+      traverse(current_path, next_caves, level+1)
+    elsif next_cave == "end"
+     $paths << path
+     # p "success path: #{path.join(',')}"
+     traverse(current_path, next_caves, level+1)
+    elsif visit_small_twice(path)
+      # p "dead path: #{path.join(',')}"
+      traverse(current_path, next_caves, level+1)
+    else
+      traverse(path, connected_caves(next_cave), level+1)
+    end
+  end
+
+  # p "current_path: #{current_path} next_caves: #{next_caves.join(',')}"
+
+  unless next_caves.empty?
+    traverse(current_path, next_caves, level+1)
+  end
+end
+
+traverse(["start"], connected_caves("start"))
+
+$paths.each {|path|
+  puts path.join(',')
+}
+
+p $paths.length
