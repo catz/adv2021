7c86f07bdc7174ddb58eba7952f2ba4b14ea7c02
Roman Truschev <truschev@gmail.com>
2021-12-24 20:00:56 +0300

    day15 solution. part1. better names

--- day15/day15.1.rb
+++ day15/day15.1.rb
@@ -3,32 +3,32 @@
 
 file = File.open("input15.txt")
 
-cave = {}
+caves = {}
 file.readlines.each_with_index.map{|line, i|
-  line.chomp.chars.each_with_index.map{|c, j|
-    cave[[i,j]] = {
-      risk: c.to_i,
-      d: Float::INFINITY,
-      prev: nil
+  line.chomp.chars.each_with_index.map{|risk, j|
+    caves[[i,j]] = {
+      risk: risk.to_i,
+      total_risk: Float::INFINITY,
+      previos_cave: nil
     }
   }
 }
 
-cave[[0,0]][:d] = 0
-unvisited = cave.keys
+caves[[0,0]][:total_risk] = 0
+unvisited = caves.keys
 
 while(!unvisited.empty?)
-  x, y = unvisited.min_by{|c| cave[c][:d] }
+  x, y = unvisited.min_by{|coord| caves[coord][:d] }
 
   cave1 = [x+1,y]
   cave2 = [x,y+1]
 
-  [cave1, cave2].each{|c|
-    if cave[c]
-      new_risk = cave[[x,y]][:d] + cave[c][:risk]
-      if new_risk < cave[c][:d]
-        cave[c][:d] = new_risk
-        cave[c][:prev] = [x,y]
+  [cave1, cave2].each{|coord|
+    if caves[coord]
+      new_risk = caves[[x,y]][:total_risk] + caves[coord][:risk]
+      if new_risk < caves[coord][:total_risk]
+        caves[coord][:total_risk] = new_risk
+        caves[coord][:previos_cave] = [x,y]
       end
     end
   }
@@ -36,6 +36,6 @@ while(!unvisited.empty?)
   unvisited.delete [x,y]
 end
 
-max = cave.keys.map{|i,j| i}.max
+max = caves.keys.map{|i,j| i}.max
 
-p cave[[max,max]][:d]
+p caves[[max,max]][:total_risk]
